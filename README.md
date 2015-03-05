dc32991423e6c6c2fe206a41bb695a87ce873227
micrum <rumiancau@gmail.com>
2015-03-05 22:54:51 +0300

    add Bootstrap

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Demo application for Ruby on Rails course
+
+Course repo https://github.com/micrum/course
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   root  'pages#home'
 
   get 'course' => 'pages#course'
-
-  get 'pages/testimonials'
+  get 'testimonials' => 'pages#testimonials'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
