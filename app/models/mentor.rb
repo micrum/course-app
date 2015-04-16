d43ba54f3b5e76fefef4b7a0eeca280828a2f22e
micrum <rumiancau@gmail.com>
2015-04-17 01:11:09 +0300

    remove unnecessary validation

--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -2,5 +2,5 @@ class Mentor < ActiveRecord::Base
   has_secure_password
 
   validates :name, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }
 end
